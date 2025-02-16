7e4f33592f426b6e155a262a8a950fc760dd989a
Jiro <j.code.public@gmail.com>
2025-02-16 15:13:19 -0800

    Add PDFs

--- _ans/rename.sh
+++ _ans/rename.sh
@@ -1,9 +1,13 @@
 
 IDX=1
-cat name.txt | while read line
+TARGET=files_1/*.pdf
+TARGET_DIR=files_1/
+NAMEFILE=_ans/pdf_file_names.txt
+
+cat $NAMEFILE | while read line
 do
-  FILENAME=$(ls -1 sample*.pdf | head -n $IDX | tail -n 1)
-  mv "$FILENAME" $line
+  FILENAME=$(ls -1 $TARGET | head -n $IDX | tail -n 1)
+  mv "$FILENAME" $TARGET_DIR$line
 
   IDX=$((IDX+1))
 done
